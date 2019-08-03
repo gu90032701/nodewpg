ee70613f43a09415ad3c82953663d8473f8b492e
gu90032701 <gu900327@hotmail.com>
2019-08-03 11:53:56 -0500

    15-17

---
+++ js/ex_15/index15.js
@@ -0,0 +1,2 @@
+const bestStudentName = "chris";
+console.log(`The best class student is: ${bestStudentName}`);
---
+++ js/ex_17/index17.js
@@ -0,0 +1,6 @@
+const seriesName = "Game of Thrones";
+const dragon1 = "Drogon";
+const dragon2 = "Rhaegal";
+const dragon3 = "Viserion";
+const template = `in ${seriesName}the dragons name are: ${dragon1},${dragon2} & ${dragon3} `;
+console.log(template);
