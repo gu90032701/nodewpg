d7120ea2a8972d81a3822cfd5c5cc25ce5df3a27
gu90032701 <gu900327@hotmail.com>
2019-08-03 12:09:49 -0500

    20-21

---
+++ js/ex_20/index20.js
@@ -0,0 +1,5 @@
+let day = "3";
+let month = "8";
+let year = "2019";
+let template = day + "/" + month + "/" + year;
+console.log(template);
---
+++ js/ex_21/index21.js
@@ -0,0 +1,7 @@
+let age = 27;
+let phone = 12345678;
+let streetName = "CMU";
+let streetNumber = "600";
+console.log(`I'm ${age} years old`);
+console.log(`My phone number is:${phone}`);
+console.log(`I live on ${streetNumber} ${streetName}`);
