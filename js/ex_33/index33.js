a3818e844bf340e16cc0994059956d1cef185c99
gu90032701 <gu900327@hotmail.com>
2019-08-11 16:31:05 -0500

    js

---
+++ js/ex_33/index33.js
@@ -0,0 +1,10 @@
+let userCount = 100;
+userCount = userCount + 5;
+console.log("Users count:" + userCount);
+userCount = userCount - 3;
+console.log("Users count:" + userCount);
+userCount *= 2;
+console.log("Users count:" + userCount);
+manCount = userCount / 2;
+womanCount = userCount / 2;
+console.log(userCount, manCount, womanCount);
