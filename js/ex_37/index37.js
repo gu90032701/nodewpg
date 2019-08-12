cb08f873c8abc7f9b0ec926f34f8b1ba2d719ef7
gu90032701 <gu900327@hotmail.com>
2019-08-11 21:46:16 -0500

    js

---
+++ js/ex_37/index37.js
@@ -0,0 +1,5 @@
+let average = 6;
+let studentAverage = 4;
+let missingpoints = Math.abs(average - studentAverage);
+console.log("pass:" + studentAverage > average);
+console.log("This student is missing " + missingpoints + " points to pass");
